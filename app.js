8e95dd7d3905f86c18e1de13a839761fe14b0fe5
KL-dev-creator <155812039+KL-dev-creator@users.noreply.github.com>
2024-08-28 00:32:52 +1200

    made a working grid using javascript

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const resetButton = document.querySelector('button')
 
 const createGrid = (amountOfGrids) => {
     //making a loop to make the rows
-    for (i = 0; i > amountOfGrids; i++) {
+    for (i = 0; i < amountOfGrids; i++) {
             // row makes a div
             const row = document.createElement('div');
             row.classList.add('gridRow')
 
             //making a loop so that the rows have columns
-            for (j= 0; j > amountOfGrids; j++) {
+            for (j= 0; j < amountOfGrids; j++) {
             // column makes a div
             const column = document.createElement('div');
             column.classList.add('gridColumn')
